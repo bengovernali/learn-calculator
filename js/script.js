f357e531af9fe2d5a30ae5e2a1b5b0b22dee53bc
Ben Governali <bgovern93@gmail.com>
2019-05-09 13:11:57 -0400

    added calculations to the result event handler

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ numbers.forEach(function(number) {
         }
     });
 });
+
 // adding click handlers to the operation buttons
 operators.forEach(function(operator) {
     operator.addEventListener('click', function() {
@@ -42,36 +43,51 @@ operators.forEach(function(operator) {
         }; 
     });
 });
+
 // on click of 'equal' button
 result.addEventListener('click', function() {
-    input.innerHTML = 'I\'m sorry, I can\'t do that Dave';
+    const currentString = input.innerHTML;
+    const numberStringArray = currentString.split(/\+|\-|\*|\//g);
+    
+    let numbersArray = [];
+    
+    numberStringArray.forEach(function(number){
+        numbersArray.push(Number(number));
+    });
+
+    const operatorsArray = currentString.replace(/[0-9]|\./g, "").split("");
+    console.log(numbersArray);
+
+    let multiply = operatorsArray.indexOf("*");
+    while (multiply != -1) {
+        numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply + 1]);
+        operatorsArray.splice(multiply, 1);
+        multiply = operatorsArray.indexOf('*');
+    }
+    let divide = operatorsArray.indexOf("/");
+    while (divide != -1) {
+        numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide + 1]);
+        operatorsArray.splice(divide, 1);
+        divide = operatorsArray.indexOf("/");
+    }
+    let add = operatorsArray.indexOf("+");
+    while (add != -1) {
+        numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add + 1]);
+        operatorsArray.splice(add, 1);
+        add = operatorsArray.indexOf("+");
+    }
+    let subtract = operatorsArray.indexOf("-");
+    while (subtract != -1) {
+        numbersArray.splice(subtract, 2, numbersArray[subtract] - numbersArray[subtract + 1]);
+        operatorsArray.splice(subtract, 1);
+        subtract = operatorsArray.indexOf("-");
+    }
+    resultDisplayed = true;
+    input.innerHTML = numbersArray;
+    console.log(numbersArray);
 });
+
 // clearing the input on press of clear
 clear.addEventListener('click', function() {
     input.innerHTML = '';
 });
-
-//.split(/\+|\-|\×|\÷/g)
-
-//From Sean's code
-
-/*Array.from(numbers).map(number => {
-    number.addEventListener('click', function() {
-        if (resultDisplayed === false) {
-            input.innerHTML += this.innerHTML;
-        } else if (
-            (resultDisplayed === true && lastChar === "+") ||
-            (resultDisplayed === true && lastChar === "-") ||
-            (resultDisplayed === true && lastChar === "*") ||
-            (resultDisplayed === true && lastChar === "/") ||
-        )
-        {
-            resultDisplayed = false;
-            input.innerHTML += this.innerHTML;
-        } else {
-            resultDisplayed = false;
-            input.innerHTML = "";
-            input.innerHTML += this.innerHTML;
-        }
-    });
-});*/
