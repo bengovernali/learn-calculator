a7334575dab0e9ff5996d6618db49d2a8da9deff
Ben Governali <bgovern93@gmail.com>
2019-05-09 10:41:07 -0400

    added conditional to numbers to check if a result has been displayed or not

--- js/script.js
+++ js/script.js
@@ -12,7 +12,19 @@ let resultDisplayed = false; // flag to keep an eye on what output is displayed
 // adding click handlers to number buttons
 numbers.forEach(function(number) {
     number.addEventListener('click', function() {
-        input.innerHTML += this.innerHTML;
+        let currentString = input.innerHTML;
+        let lastChar = currentString[currentString.length -1];
+        if (resultDisplayed === false) {
+            input.innerHTML += this.innerHTML;
+        } else if (
+            (resultDisplayed === true) && (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/")) {
+            resultDisplayed = false;
+            input.innerHTML += this.innerHTML;
+        } else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+        }
     });
 });
 // adding click handlers to the operation buttons
@@ -20,8 +32,14 @@ operators.forEach(function(operator) {
     operator.addEventListener('click', function() {
         if (ops.includes(input.innerHTML[input.innerHTML.length -1])) {
             input.innerHTML= input.innerHTML.slice(0, input.innerHTML.length - 1);
+            input.innerHTML += this.innerHTML;
+        } 
+        else if (input.innerHTML.length === 0) {
+            console.log("enter a number first");
+        }
+        else {
+            input.innerHTML += this.innerHTML;
         }; 
-        input.innerHTML += this.innerHTML; 
     });
 });
 // on click of 'equal' button
@@ -34,3 +52,26 @@ clear.addEventListener('click', function() {
 });
 
 //.split(/\+|\-|\×|\÷/g)
+
+//From Sean's code
+
+/*Array.from(numbers).map(number => {
+    number.addEventListener('click', function() {
+        if (resultDisplayed === false) {
+            input.innerHTML += this.innerHTML;
+        } else if (
+            (resultDisplayed === true && lastChar === "+") ||
+            (resultDisplayed === true && lastChar === "-") ||
+            (resultDisplayed === true && lastChar === "*") ||
+            (resultDisplayed === true && lastChar === "/") ||
+        )
+        {
+            resultDisplayed = false;
+            input.innerHTML += this.innerHTML;
+        } else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+        }
+    });
+});*/
